2a21f8ac531a497d48fd99e69f5711e8c776dfee
Daisuke Yamashita <daisuke@cookpad.com>
2013-02-21 11:06:13 +0900

    rails new abtesting-split

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AbtestingSplit::Application.config.secret_token = '78556f3747b8a32fb13c7d5b7eb9c89ca01de95f08a0e2a014e01fc29f5c77ba60962d392d0be37075832f75ac8f42f4bac240e5336ec4976e59a20fc9610643'
